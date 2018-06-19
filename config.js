409f99b983a290ed38105d693aac2da59f5b6713
cooyzee <cooyzee@gmail.com>
2018-06-19 10:36:36 +0800

    Project's init.

---
+++ config.js
@@ -0,0 +1,12 @@
+// project's configuration
+exports.version = '1.0.0'
+
+exports.development = {
+  redisSever: '192.168.0.250',
+  redisSeverPorts: [6000, 6001, 6002, 6003, 6004, 6005],
+  token: 'WeChatServer4SXB',
+  appId: 'wx44bc57e236e7ed54',
+  appSecret: '418bf768ff524fef0570f97004753817',
+  sxbShop: 'http://shop.local.sxbcar.com',
+  templateId: 'oGVsWeTa28_oRMpSFWFow0_3dCewE69IIQxQ3uHEsUQ'
+}
